27b8bd8f657205f0b1e6fde87c56982cd39fda95
mmx369 <maksim@klishevich.com>
2021-04-16 00:21:05 +0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/client'
+
+export default function Header() {
+  const [session] = useSession()
+
+  console.log('SESSION', session)
+
+  const handleSignin = (e) => {
+    e.preventDefault()
+    signIn()
+  }
+  const handleSignout = (e) => {
+    e.preventDefault()
+    signOut()
+  }
+
+  return (
+    <div className="header">
+      <Link href="/">
+        <a className="logo">NextAuth.js</a>
+      </Link>
+      {session && (
+        <a href="#" onClick={handleSignout} className="btn-signin">
+          Sign out
+        </a>
+      )}
+      {!session && (
+        <a href="#" onClick={handleSignin} className="btn-signin">
+          Sign in
+        </a>
+      )}
+    </div>
+  )
+}
